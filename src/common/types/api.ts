68ae70776d9a7611a1053df68745c2c6cb795bbf
iigorfelipe <iigorfelipe@gmail.com>
2025-08-09 22:28:52 -0300

    feat: add typing for kingdoms and  species details

--- src/common/types/api.ts
+++ src/common/types/api.ts
@@ -7,10 +7,38 @@ export type Rank =
   | "GENUS"
   | "SPECIES";
 
+export type Kingdom =
+  | "animalia"
+  | "archaea"
+  | "bacteria"
+  | "chromista"
+  | "fungi"
+  | "protozoa"
+  | "plantae"
+  | "viruses";
+
 export interface Taxon {
   key: number;
   scientificName: string;
   rank: Rank;
-  kingdom: string;
+  kingdom: Kingdom;
   canonicalName: string;
+  numDescendants: number;
+  class: string;
+  taxonomicStatus: string;
 }
+
+export type SpecieDetail = {
+  canonicalName: string;
+  scientificName: string;
+  kingdom: Kingdom;
+  phylum: string;
+  class: string;
+  order: string;
+  family: string;
+  genus: string;
+  species: string;
+  authorship: string;
+  publishedIn: string;
+  title: string;
+};
