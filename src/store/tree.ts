39130d6990f5fd2e2d031339c8da812b634af09a
iigorfelipe <iigorfelipe@gmail.com>
2025-08-09 22:49:06 -0300

    feat: add Jotai store for tree data shared across project

--- src/store/tree.ts
+++ src/store/tree.ts
@@ -1,6 +1,53 @@
+import type { Kingdom, Rank } from "@/common/types/api";
+import { dataFake } from "@/common/utils/dataFake";
 import { atom } from "jotai";
 
-export const specieKeyAtom = atom<number | null>(null);
+export type PathNode = {
+  rank: Rank;
+  key: number;
+  kingdom: Kingdom;
+};
 
-// Map: rank -> key expandido
-export const expandedNodeAtomByRank = atom<Record<string, string | null>>({});
+const expandedNodes = atom<PathNode[]>([]);
+
+export type ChallengeMode = "DAILY" | "RANDOM" | null;
+type ChallengeStatus = "NOT_STARTED" | "IN_PROGRESS" | "COMPLETED";
+
+type Challenge = {
+  mode: ChallengeMode;
+  status: ChallengeStatus;
+};
+
+const challenge = atom<Challenge>({ mode: null, status: "NOT_STARTED" });
+
+export type ExploreInfo = {
+  kingdomKey: number;
+  kingdomName: Kingdom;
+  numDescendants: number;
+  explored: number;
+  icon: string;
+  primaryColor: string;
+  lightColor: string;
+  description: string;
+  mainGroups: { groupName: string; pathNode: PathNode[] }[];
+};
+
+const exploreInfos = atom<ExploreInfo[]>(dataFake as ExploreInfo[]);
+const treeScroll = atom({
+  autoScroll: false,
+  scrollIndex: 0,
+  loadingMap: {},
+});
+
+const animate = atom({
+  isShaking: false,
+  shakeDirection: true,
+});
+
+export const treeAtom = {
+  expandedNodes,
+  challenge,
+  exploreInfos,
+  treeScroll,
+  animate,
+};
