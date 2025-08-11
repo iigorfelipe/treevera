0e645625d7d395169bb1bf17f6abf06d71617d3d
iigorfelipe <iigorfelipe@gmail.com>
2025-08-11 17:21:58 -0300

    refactor: remove unnecessary code

--- src/pages/tree.tsx
+++ src/pages/tree.tsx
@@ -3,25 +3,6 @@ import "./tree.css";
 import { TreeNode } from "@/modules/tree/tree-node";
 import { TreeSkeleton } from "@/modules/tree/components/tree-skeleton";
 
-// const Filters = () => {
-//   return (
-//     <div className="flex max-w-2xs flex-col gap-4">
-//       <div className="group flex h-12 w-full items-center gap-4 rounded-xl border px-4 py-3 text-sm">
-//         <img
-//           src="./assets/search-icon.png"
-//           className="h-full w-fit rotate-y-180 group-hover:scale-110 group-hover:rotate-5"
-//           alt=""
-//         />
-//         <input
-//           type="text"
-//           placeholder="Buscar espécie..."
-//           className="outline-none placeholder:font-medium"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
 export const Tree = () => {
   const { data: kingdoms, isLoading } = useGetKingdoms();
 
@@ -34,8 +15,6 @@ export const Tree = () => {
       className="flex h-full w-full flex-col gap-6 overflow-auto px-4 py-28"
       style={{ WebkitOverflowScrolling: "touch" }}
     >
-      {/* <Filters /> */}
-
       <ul>
         {kingdoms.map((kingdom) => (
           <TreeNode key={kingdom.key} taxon={kingdom} />
