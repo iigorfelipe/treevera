3044d8ae1ed377c93f78fd2116f10d4c3ab64e78
iigorfelipe <iigorfelipe@gmail.com>
2025-08-11 10:13:50 -0300

    refactor: remove future features from menu and add button animation

--- src/modules/header/menu.tsx
+++ src/modules/header/menu.tsx
@@ -1,5 +1,5 @@
 import { useTheme } from "@/hooks/theme";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -15,37 +15,39 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Settings, User as UserIcon } from "lucide-react";
+import { Settings } from "lucide-react";
 
 export const Menu = () => {
   const { changeTheme, theme } = useTheme();
 
-  const { i18n } = useTranslation();
+  // const { i18n } = useTranslation();
 
   const user = false;
 
   return (
-    <div className="absolute bottom-2.5 z-50 w-[calc(100%-8px)] rounded-tr-2xl p-3 backdrop-blur-3xl">
+    <div className="group absolute bottom-0 z-50 overflow-hidden rounded-tr-3xl p-3 backdrop-blur-2xl">
+      <span className="bg-accent-foreground absolute inset-0 origin-center scale-0 rounded-full transition-transform delay-500 duration-500 group-hover:scale-100 group-hover:delay-0"></span>
+
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <button
-            className="group flex cursor-pointer items-center justify-center rounded-xl transition-colors"
-            aria-label="Configurações"
-          >
-            <div className="bg-accent flex size-10 items-center justify-center rounded-full">
-              {user ? <span className="text-xl">I</span> : <Settings />}
-            </div>
+          <button className="bg-accent-foreground text-accent relative flex size-8 cursor-pointer items-center justify-center rounded-full">
+            <span className="pointer-events-none absolute inset-0 scale-0 rounded-full bg-inherit transition-transform duration-300 ease-in-out group-hover:scale-150" />
+
+            {user ? (
+              <span className="text-xl">I</span>
+            ) : (
+              <Settings className="size-5 transition-transform delay-300 duration-300 ease-in-out group-hover:scale-150 group-hover:delay-0" />
+            )}
           </button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuContent align="start" className="w-56">
           <DropdownMenuLabel className="flex items-center gap-2">
-            <UserIcon className="size-4" />
-            Perfil
+            Configurações
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
             <div className="flex w-full flex-col gap-1">
-              <span className="text-muted-foreground mb-1 text-xs">Tema</span>
+              <span className="mb-1 text-xs">Tema</span>
               <Select onValueChange={changeTheme} value={theme}>
                 <SelectTrigger className="h-8 w-full rounded-md border px-2 text-sm font-medium">
                   <SelectValue />
@@ -58,7 +60,7 @@ export const Menu = () => {
               </Select>
             </div>
           </DropdownMenuItem>
-          <DropdownMenuItem asChild>
+          {/* <DropdownMenuItem asChild>
             <div className="flex w-full flex-col gap-1">
               <span className="text-muted-foreground mb-1 text-xs">Idioma</span>
               <Select
@@ -75,7 +77,7 @@ export const Menu = () => {
                 </SelectContent>
               </Select>
             </div>
-          </DropdownMenuItem>
+          </DropdownMenuItem> */}
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
