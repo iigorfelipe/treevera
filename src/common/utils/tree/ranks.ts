93604312a2e977d37eb2fc75350fc957725ede69
iigorfelipe <iigorfelipe@gmail.com>
2025-09-15 11:27:41 -0300

    feat: adds utility functions related to taxonomic rank

---
+++ src/common/utils/tree/ranks.ts
@@ -0,0 +1,74 @@
+import animaliaUrl from "@/assets/images/avif-animalia.avif?url";
+import archaeaUrl from "@/assets/images/avif-archaea.avif?url";
+import bacteriaUrl from "@/assets/images/avif-bacteria.avif?url";
+import chromistaUrl from "@/assets/images/avif-chromista.avif?url";
+import fungiUrl from "@/assets/images/avif-fungi.avif?url";
+import plantaeUrl from "@/assets/images/avif-plantae.avif?url";
+import protozoaUrl from "@/assets/images/avif-protozoa.avif?url";
+
+import type { Rank, Taxon } from "../../types/api";
+import { capitalizar } from "../string";
+
+export const RANK_ORDER: Rank[] = [
+  "KINGDOM",
+  "PHYLUM",
+  "CLASS",
+  "ORDER",
+  "FAMILY",
+  "GENUS",
+  "SPECIES",
+];
+
+export const getNextRank = (currentRank: string): Rank | null => {
+  const index = RANK_ORDER.indexOf(currentRank.toUpperCase() as Rank);
+
+  if (index >= 0 && index < RANK_ORDER.length - 1) {
+    return RANK_ORDER[index + 1];
+  }
+  return null;
+};
+
+export const RANK_FIXES: Record<string, string> = {
+  Crocodylia: "Order",
+  Squamata: "Order",
+  Testudines: "Order",
+  Sphenodontia: "Order",
+};
+
+export const getDisplayRank = (taxon: Taxon) => {
+  const realRank = capitalizar(taxon.rank);
+  const fixRank = RANK_FIXES[taxon.canonicalName || taxon.scientificName];
+
+  if (fixRank && fixRank.toUpperCase() !== realRank.toUpperCase()) {
+    return {
+      name: fixRank,
+      icon: true,
+    };
+  }
+
+  return {
+    name: realRank,
+    icon: false,
+  };
+};
+
+export const getRankIcon = (kingdomKey: number) => {
+  switch (kingdomKey) {
+    case 1:
+      return animaliaUrl;
+    case 2:
+      return archaeaUrl;
+    case 3:
+      return bacteriaUrl;
+    case 4:
+      return chromistaUrl;
+    case 5:
+      return fungiUrl;
+    case 6:
+      return plantaeUrl;
+    case 7:
+      return protozoaUrl;
+    default:
+      return plantaeUrl;
+  }
+};
