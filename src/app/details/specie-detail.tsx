4b3c47ecc26271a59893a65dd8ecac30cd4e2ba9
iigorfelipe <iigorfelipe@gmail.com>
2025-11-27 23:26:27 -0300

    feat: update SpecieDetail to include species occurrence map

--- src/app/details/specie-detail.tsx
+++ src/app/details/specie-detail.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/modules/specie-detail/skeletons";
 import { treeAtom } from "@/store/tree";
 import { useAtomValue } from "jotai";
+import { OccurrenceMap } from "@/modules/specie-detail/occurrences-map";
 
 export const SpecieDetail = () => {
   const specieKey = useAtomValue(treeAtom.expandedNodes).find(
@@ -30,9 +31,12 @@ export const SpecieDetail = () => {
 
   return (
     <div style={{ containerType: "inline-size" }}>
-      <div className="m-4 mt-28 grid grid-cols-1 gap-6 py-4 pr-[1px] pl-4 md:mt-4 [@container(min-width:820px)]:grid-cols-2">
+      <div className="m-4 mt-28 grid grid-cols-1 gap-6 py-4 pr-px pl-4 md:mt-4 [@container(min-width:820px)]:grid-cols-2">
         <SpecieInfos />
-        <SpecieImageDetail />
+        <div className="flex flex-col gap-6">
+          <SpecieImageDetail />
+          {specieKey && <OccurrenceMap specieKey={specieKey} />}
+        </div>
       </div>
     </div>
   );
