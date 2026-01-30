9ec572003b239fe55e7617ce7fda8868940943e5
iigor <iigorfelipe@gmail.com>
2026-01-29 23:24:16 -0300

    feat: update status in DailyChallenge

--- src/app/auth/challenge.tsx
+++ src/app/auth/challenge.tsx
@@ -1,21 +1,23 @@
+import { Image } from "@/common/components/image";
 import { Button } from "@/common/components/ui/button";
 import { Card, CardContent } from "@/common/components/ui/card";
 import { cn } from "@/common/utils/cn";
-import { ChallengeBtn } from "@/modules/challenge/challenge-btn";
+import Alvo from "@/assets/alvo.gif";
 import { TaxonomicPath } from "@/modules/challenge/taxonomic-path";
 import { authStore } from "@/store/auth";
 
 import { treeAtom } from "@/store/tree";
 import { useNavigate } from "@tanstack/react-router";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
+import { getDailySpecies } from "@/common/utils/game/daily-species";
 
 export const DailyChallenge = () => {
   const [challenge, setChallenge] = useAtom(treeAtom.challenge);
-  const [viewChallenge, setViewChallenge] = useState(false);
   const setExpandedNodes = useSetAtom(treeAtom.expandedNodes);
   const isAuthenticated = useAtomValue(authStore.isAuthenticated);
   const navigate = useNavigate();
+  const speciesName = getDailySpecies();
 
   const inProgress = useMemo(
     () => challenge.status === "IN_PROGRESS",
@@ -27,6 +29,7 @@ export const DailyChallenge = () => {
       setChallenge((prev) => ({
         ...prev,
         status: "NOT_STARTED",
+        mode: "UNSET"
       }));
       return;
     }
@@ -39,6 +42,7 @@ export const DailyChallenge = () => {
     setChallenge((prev) => ({
       ...prev,
       status: "IN_PROGRESS",
+      mode: "DAILY"
     }));
     setExpandedNodes([]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -46,16 +50,22 @@ export const DailyChallenge = () => {
 
   return (
     <div className="px-4 py-6">
-      <Card
-        className={cn(
-          "mx-auto w-full max-w-7xl rounded-2xl border bg-transparent shadow-sm",
-          !viewChallenge && "cursor-pointer",
-        )}
-        onClick={() => setViewChallenge(true)}
-      >
+      <Card className="mx-auto rounded-2xl border bg-transparent shadow-sm">
         <CardContent className="flex flex-col gap-6">
           <div className="flex items-center justify-between">
-            <ChallengeBtn />
+            
+            <div className="group flex items-center font-semibold">
+              <Image src={Alvo} className="size-12" alt="Alvo gif" />
+              <div className="flex flex-col items-start">
+                <h2 className="text-xl font-bold 2xl:text-2xl">Desafio Diário</h2>
+                <p>
+                  Encontre:{" "}
+                  <i className="font-semibold text-emerald-600 dark:text-green-500">
+                    {speciesName}
+                  </i>
+                </p>
+              </div>
+            </div>
 
             <div className="text-right">
               <div className="mb-1 text-sm">Tempo restante</div>
@@ -63,39 +73,36 @@ export const DailyChallenge = () => {
             </div>
           </div>
 
-          {viewChallenge && (
-            <>
-              {inProgress && <TaxonomicPath />}
+          {inProgress && <TaxonomicPath />}
 
-              <div className="mb-6 grid grid-cols-3 gap-4">
-                <div className="rounded-xl border p-4 text-center">
-                  <div className="text-2xl font-bold">+100</div>
-                  <div className="text-sm">XP Base</div>
-                </div>
-                <div className="rounded-xl border p-4 text-center">
-                  <div className="text-2xl font-bold">+50</div>
-                  <div className="text-sm">Precisão</div>
-                </div>
-                <div className="rounded-xl border p-4 text-center">
-                  <div className="text-2xl font-bold">+25</div>
-                  <div className="text-sm">Velocidade</div>
-                </div>
-              </div>
+          {/* <div className="mb-6 grid grid-cols-3 gap-4">
+            <div className="rounded-xl border p-4 text-center">
+              <div className="text-2xl font-bold">+100</div>
+              <div className="text-sm">XP Base</div>
+            </div>
+            <div className="rounded-xl border p-4 text-center">
+              <div className="text-2xl font-bold">+50</div>
+              <div className="text-sm">Precisão</div>
+            </div>
+            <div className="rounded-xl border p-4 text-center">
+              <div className="text-2xl font-bold">+25</div>
+              <div className="text-sm">Velocidade</div>
+            </div>
+          </div> */}
 
-              <Button
-                size="lg"
-                className={cn(
-                  "cursor-pointer bg-emerald-600 p-6 hover:bg-emerald-600/90",
-                  inProgress && "bg-red-500 p-6 hover:bg-red-500/90",
-                )}
-                onClick={handleCkick}
-              >
-                <span className="text-xl">
-                  {inProgress ? "Cancelar Desafio" : "Iniciar Desafio"}
-                </span>
-              </Button>
-            </>
-          )}
+          <Button
+            size="lg"
+            className={cn(
+              "cursor-pointer bg-emerald-600 p-6 hover:bg-emerald-600/90",
+              inProgress && "bg-red-500 p-6 hover:bg-red-500/90",
+            )}
+            onClick={handleCkick}
+          >
+            <span className="text-xl">
+              {inProgress ? "Cancelar Desafio" : "Iniciar Desafio"}
+            </span>
+          </Button>
+ 
         </CardContent>
       </Card>
     </div>
