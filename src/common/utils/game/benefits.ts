1742c9e4c11f96a2a87ec117845b776539fbab01
iigor <iigorfelipe@gmail.com>
2026-03-02 01:05:48 -0300

    chore: remove information about unimplemented features

--- src/common/utils/game/benefits.ts
+++ src/common/utils/game/benefits.ts
@@ -1,55 +1,62 @@
-import {
-  Award,
-  Images,
-  Crown,
-  GitBranchPlus,
-  TrendingUp,
-  Trophy,
-} from "lucide-react";
+import { Images, Target, GitBranchPlus, ListTree, Eye } from "lucide-react";
 
 export const benefits = [
-  {
-    icon: Images,
-    title: "Galeria de Espécies",
-    description: "Organize e favorite suas espécies já descobertas",
-    color: "text-green-600",
-    bgColor: "bg-green-50",
-  },
-  {
-    icon: Trophy,
-    title: "Desafios Diários",
-    description: "Novos desafios todos os dias para testar seus conhecimentos",
-    color: "text-orange-600",
-    bgColor: "bg-orange-50",
-  },
-  {
-    icon: Award,
-    title: "Conquistas e Medalhas",
-    description: "Desbloqueie conquistas especiais e mostre suas habilidades",
-    color: "text-pink-600",
-    bgColor: "bg-pink-50",
-  },
-
-  {
-    icon: Crown,
-    title: "Ranking Global",
-    description: "Compita com outros exploradores da taxonomia",
-    color: "text-yellow-600",
-    bgColor: "bg-yellow-50",
-  },
-
   {
     icon: GitBranchPlus,
     title: "Salvar atalhos",
     description: "Defina nós mais visitados para agilizar sua navegação",
-    color: "text-blue-600",
-    bgColor: "bg-blue-50",
+    color: "text-purple-600",
+    bgColor: "",
   },
   {
-    icon: TrendingUp,
-    title: "Acompanhe seu Progresso",
-    description: "Veja suas estatísticas e evolução ao longo do tempo",
-    color: "text-purple-600",
-    bgColor: "bg-purple-50",
+    icon: Target,
+    title: "Desafios Diários e Aleatórios",
+    description: "Novos desafios todos os dias para testar seus conhecimentos",
+    color: "text-green-600",
+    bgColor: "",
   },
+  {
+    icon: Eye,
+    title: "Historico de visualizações",
+    description: "Acompanhe as espécies que você já explorou",
+    color: "text-yellow-600",
+    bgColor: "",
+  },
+  {
+    icon: Images,
+    title: "Galeria de Espécies",
+    description: "Organize e favorite suas espécies já descobertas",
+    color: "text-pink-600",
+    bgColor: "",
+  },
+  {
+    icon: ListTree,
+    title: "Historico de atividades",
+    description: "Veja um resumo das suas interações na plataforma",
+    color: "text-blue-600",
+    bgColor: "",
+  },
+
+  // {
+  //   icon: Award,
+  //   title: "Conquistas e Medalhas",
+  //   description: "Desbloqueie conquistas especiais e mostre suas habilidades",
+  //   color: "text-pink-600",
+  //   bgColor: "",
+  // },
+
+  // {
+  //   icon: Crown,
+  //   title: "Ranking Global",
+  //   description: "Compita com outros exploradores da taxonomia",
+  //   color: "text-yellow-600",
+  //   bgColor: "",
+  // },
+  // {
+  //   icon: TrendingUp,
+  //   title: "Acompanhe seu Progresso",
+  //   description: "Veja suas estatísticas e evolução ao longo do tempo",
+  //   color: "text-purple-600",
+  //   bgColor: "",
+  // },
 ];
