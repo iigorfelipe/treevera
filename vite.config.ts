0001234cc52d47086baf480882f51facb802ddec
iigor <iigorfelipe@gmail.com>
2026-04-17 10:13:05 -0300

    remove ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  const isGhPages = mode === "gh-pages";
-  return {
-    plugins: [react(), tailwindcss()],
-    base: isGhPages ? "/treevera/" : "/",
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-  };
+  },
 });
